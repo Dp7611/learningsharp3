72524be501c74893bc2bb1df6206e9158df93560
Dp7611 <Dp761@student.aru.ac.uk>
2023-01-26 13:48:32 +0000

    Created swapNumbers

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace learningsharp3;
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadKey(true);
         
+            swapNumbers();
     }
+
+    static void swapNumbers()
+    {
+        string num1, num2, temp;
+
+        Console.WriteLine("Input the first number: ");
+        num1 = Console.ReadLine();
+        Console.WriteLine("Input the second number: ");
+        num2 = Console.ReadLine();
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        Console.WriteLine("After Swapping :");
+        Console.WriteLine($"First Number : {num1}");
+        Console.WriteLine($"Second Number : {num2}");
+        Console.ReadKey(true); 
+        }
 }
